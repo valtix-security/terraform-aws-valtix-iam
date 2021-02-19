60e1578605554afb14fd68947d3bc06b7ceb7358
eddievaltix <eddie@valtix.com>
2021-02-19 06:14:39 +0000

    controller role update

--- controller_role.tf
+++ controller_role.tf
@@ -35,16 +35,104 @@ resource "aws_iam_role_policy" "valtix_controller_policy" {
   "Version": "2012-10-17",
   "Statement": [
     {
+        "Effect": "Allow",
         "Action": [
-          "apigateway:GET",
-          "ec2:*",
-          "elasticloadbalancing:*",
-          "route53:ListHostedZones",
-          "route53:ChangeResourceRecordSets",
-          "route53:ListResourceRecordSets",
-          "route53:ListHostedZonesByName",
-          "servicequotas:GetServiceQuota"
+            "ec2:Describe*",
+            "ec2:Get*"
         ],
+        "Resource": "*"
+    },
+    {
+        "Effect": "Allow",
+        "Action": [
+            "ec2:RunInstances",
+            "ec2:RequestSpotInstances",
+            "ec2:StopInstances",
+            "ec2:StartInstances",
+            "ec2:ModifyInstanceAttribute",
+            "ec2:TerminateInstances",
+            "ec2:CancelSpotInstanceRequests",
+            "ec2:StopInstances",
+            "ec2:AttachNetworkInterface",
+            "ec2:CreateNetworkInterface",
+            "ec2:ModifyNetworkInterfaceAttribute",
+            "ec2:DeleteNetworkInterface",
+            "ec2:ReleaseAddress",
+            "ec2:DisassociateAddress",
+            "ec2:AllocateAddress",
+            "ec2:AssociateAddress",
+            "ec2:DisassociateIamInstanceProfile",
+            "ec2:AssociateIamInstanceProfile",
+            "ec2:ReplaceIamInstanceProfileAssociation",
+            "ec2:CreateTags",
+            "ec2:DeleteTags",
+            "ec2:GetConsoleOutput"
+        ],
+        "Resource": "*"
+    },
+    {
+        "Effect": "Allow",
+        "Action": [
+            "ec2:ReplaceRoute",
+            "ec2:CreateRoute",
+            "ec2:CreateRouteTable",
+            "ec2:AssociateRouteTable",
+            "ec2:DeleteRouteTable"
+        ],
+        "Resource": "*"
+    },
+    {
+        "Effect": "Allow",
+        "Action": [
+            "ec2:CreateSubnet",
+            "ec2:CreateVpc",
+            "ec2:DeleteSubnet",
+            "ec2:DeleteVpc",
+            "ec2:AssociateVpcCidrBlock",
+            "ec2:ModifySubnetAttribute",
+            "ec2:CreateSecurityGroup",
+            "ec2:DeleteSecurityGroup",
+            "ec2:CreateInternetGateway",
+            "ec2:DetachInternetGateway",
+            "ec2:DeleteInternetGateway",
+            "ec2:AttachInternetGateway",
+            "ec2:CreateVpcEndpointServiceConfiguration",
+            "ec2:DeleteVpcEndpointServiceConfigurations",
+            "ec2:CreateVpcEndpoint",
+            "ec2:DeleteVpcEndpoints",
+            "ec2:CreateTransitGateway",
+            "ec2:SearchTransitGatewayRoutes",
+            "ec2:ReplaceTransitGatewayRoute",
+            "ec2:CreateTransitGatewayRoute",
+            "ec2:AssociateTransitGatewayRouteTable",
+            "ec2:CreateTransitGatewayVpcAttachment",
+            "ec2:CreateTransitGatewayRouteTable",
+            "ec2:DeleteTransitGatewayVpcAttachment",
+            "ec2:DeleteTransitGatewayRouteTable",
+            "ec2:DisassociateTransitGatewayRouteTable",
+            "ec2:AuthorizeSecurityGroupIngress"
+        ],
+        "Resource": "*"
+    },
+    {
+        "Effect": "Allow",
+        "Action": [
+            "elasticloadbalancing:Describe*",
+            "elasticloadbalancing:Get*",
+            "elasticloadbalancing:DeleteListener",
+            "elasticloadbalancing:DeleteTargetGroup",
+            "elasticloadbalancing:DeleteLoadBalancer",
+            "elasticloadbalancing:RegisterTargets",
+            "elasticloadbalancing:DeregisterTargets",
+            "elasticloadbalancing:ModifyLoadBalancerAttributes",
+            "elasticloadbalancing:CreateLoadBalancer",
+            "elasticloadbalancing:CreateTargetGroup",
+            "elasticloadbalancing:CreateListener"
+        ],
+        "Resource": "*"
+    },
+    {
+        "Action": "servicequotas:GetServiceQuota",
         "Effect": "Allow",
         "Resource": "*"
     },
